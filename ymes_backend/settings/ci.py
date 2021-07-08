c2ec0f0d87b22d86488bbac1b32df6a2a6e6d4db
Klement Omeri <klementomeri97@gmail.com>
2021-07-09 01:04:16 +0200

    ci: add github actions

--- ymes_backend/settings/ci.py
+++ ymes_backend/settings/ci.py
@@ -1,2 +1,17 @@
 # noinspection PyUnresolvedReferences
 from ymes_backend.settings.base import *
+
+SECRET_KEY = 'ci/cd'
+AUTH_PASSWORD_VALIDATORS = []
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github_actions',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
