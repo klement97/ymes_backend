2898aae8667b013dcc73dc8a2f61467dcc38d8fa
Klement <klementomeri97@gmail.com>
2021-07-10 20:02:51 +0200

    feat: add child to django admin

--- apps/education/models/child.py
+++ apps/education/models/child.py
@@ -9,6 +9,8 @@ from apps.common.utils import Gender
 class Child(models.Model):
     first_name = models.CharField(_('First Name'), max_length=50)
     last_name = models.CharField(_('Last Name'), max_length=50)
+    mother_name = models.CharField(_('Mother name'), max_length=50)
+    father_name = models.CharField(_('Father name'), max_length=50)
     birthplace = models.CharField(_('Birthplace'), max_length=100)
     birthdate = models.DateField(_('Birthdate'))
     gender = models.CharField(max_length=1, choices=Gender.choices)
@@ -22,9 +24,13 @@ class Child(models.Model):
         verbose_name=_('Group'),
         to=Group,
         on_delete=models.SET_NULL,
+        null=True
     )
 
     class Meta:
         verbose_name = 'Child'
         verbose_name_plural = 'Children'
         db_table = 'child'
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.father_name} {self.last_name}'
