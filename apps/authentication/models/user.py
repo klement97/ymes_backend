d0c7275fe50546d11ca4f75932b5259228093562
Klement <klementomeri97@gmail.com>
2021-07-10 19:43:59 +0200

    feat(auth): edit user model

--- apps/authentication/models/user.py
+++ apps/authentication/models/user.py
@@ -2,16 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from apps.authentication.managers import UserManager
+from apps.common.utils import Gender
 
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, db_index=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices)
+    residence = models.CharField(max_length=100, null=True)
+    emergency_number = models.CharField(max_length=20, null=True)
     username = None
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        db_table = 'user'
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
